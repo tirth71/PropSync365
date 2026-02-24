de0638b2eb2310fc99cbde6f6b784aae54d73631
nikitsorathiya <nikitsorathiya@gmail.com>
2026-02-24 16:31:16 +0530

    logo

--- property365/config.php
+++ property365/config.php
@@ -1,4 +1,4 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","newproperty365_db") or die("please Check your connection!!!");
+$con = mysqli_connect("localhost:3307","root","","newproperty365_db") or die("please Check your connection!!!");
 ?>
