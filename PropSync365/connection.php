629fba9553bff865afa92f7b2c82471c7e313bd4
nikitsorathiya <nikitsorathiya@gmail.com>
2026-02-24 17:12:34 +0530

    name and logo
    some nessasry changes

---
+++ PropSync365/connection.php
@@ -0,0 +1,4 @@
+<?php 
+$con = mysqli_connect("localhost:3307","root","","newproperty365_db") or die("please Check your connection!!!");
+
+?>
--- property365/connection.php
+++
@@ -1,4 +0,0 @@
-<?php 
-$con = mysqli_connect("localhost","root","","newproperty365_db") or die("please Check your connection!!!");
-
-?>
