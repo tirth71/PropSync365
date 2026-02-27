1e0653df322bd45602327a0974f132ed2f267efd
Tirthsavaliya7 <159606779+Tirthsavaliya7@users.noreply.github.com>
2026-02-27 17:49:51 +0530

    map-google

--- PropSync365/footer.php
+++ PropSync365/footer.php
@@ -1,85 +1,85 @@
-  <footer id="footer" class="footer footer-1 bg-white">
+<div class="footer-widget"> 
+    <div class="container">
+        <div class="row">
 
-    
-            <!-- Widget Section
-	============================================= -->
-    <?php 
-        if(isset($_POST['submitn']))
-        {
-            echo "<script>alert('you subscribed us successfully..!!we updated you on this email.');</script>";
-        }
-     ?>
-            <div class="footer-widget"> 
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-12 col-sm-6 col-md-3 widget--about">
-                            <div class="widget--content">
-                                <div class="footer--logo">
-                                    <img src="assets/images/logo/newlogo.png" alt="logo">
-                                </div>
-                                <p>B-13, Property Real Estate, Silver Hub, Adajan, Surat</p>
-                                <div class="footer--contact">
-                                    <ul class="list-unstyled mb-0">
-                                        <li>+91 98253-45651</li>
-                                        <li>propsync365@gmail.com</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- .col-md-2 end -->
-                        <div class="col-xs-12 col-sm-3 col-md-2 col-md-offset-1 widget--links">
-                            <div class="widget--title">
-                                <h5>Company</h5>
-                            </div>
-                            <div class="widget--content">
-                                <ul class="list-unstyled mb-0">
-                                    <li><a href="properties-list.php">Properties</a></li>
-                                    <li><a href="page-contact.php">Contact</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <!-- .col-md-2 end -->
-                        <div class="col-xs-12 col-sm-3 col-md-2 widget--links">
-                           
-                        </div>
-                        <!-- .col-md-2 end -->
-                        <div class="col-xs-12 col-sm-12 col-md-4 widget--newsletter">
-                            <div class="widget--title">
-                                <h5>newsletter</h5>
-                            </div>
-                            <div class="widget--content">
-                                <form class="newsletter--form mb-40" method="post">
-                                    <input type="email" class="form-control" id="newsletter-email" placeholder="Email Address">
-                                    <button type="submit" name="submitn"><i class="fa fa-arrow-right"></i></button>
-                                </form>
-                               
-                            </div>
-                        </div>
-                        <!-- .col-md-4 end -->
+            <!-- 1️⃣ COMPANY DETAILS -->
+            <div class="col-xs-12 col-sm-6 col-md-3 widget--about">
+                <div class="widget--content">
+                    <div class="footer--logo">
+                        <img src="assets/images/logo/newlogo.png" alt="logo" style="max-width:180px;">
+                    </div>
 
+                    <h5 style="margin-top:15px;">Head Office</h5>
+                    <p>
+                        B-13, Property Real Estate,<br>
+                        Silver Hub, Adajan,<br>
+                        Surat, Gujarat
+                    </p>
+
+                    <div class="footer--contact">
+                        <ul class="list-unstyled mb-0">
+                            <li><b>Phone:</b> +91 98253-45651</li>
+                            <li><b>Email:</b> propsync365@gmail.com</li>
+                        </ul>
                     </div>
                 </div>
-                <!-- .container end -->
             </div>
-            <!-- .footer-widget end -->
 
-            <!-- Copyrights
-	============================================= -->
-            <div class="footer--copyright text-center">
-                <div class="container">
-                    <div class="row footer--bar">
-                        <div class="col-xs-12 col-sm-12 col-md-12">
-                            <span> 2026 PropSync365, All Rights Reserved.</span>
-                        </div>
 
-                    </div>
-                    <!-- .row end -->
+            <!-- 2️⃣ GOOGLE MAP -->
+            <div class="col-xs-12 col-sm-6 col-md-3">
+                <div class="widget--title">
+                    <h5>Our Location</h5>
                 </div>
-                <!-- .container end -->
+
+                <div style="border-radius:10px; overflow:hidden; box-shadow:0 0 12px rgba(0,0,0,0.2);">
+                    <iframe 
+                        src="https://www.google.com/maps?q=Adajan,Surat&output=embed"
+                        width="100%" 
+                        height="230" 
+                        style="border:0;"
+                        loading="lazy">
+                    </iframe>
+                </div>
             </div>
-            <!-- .footer-copyright end -->
-        </footer>
+
+
+            <!-- 3️⃣ QUICK LINKS -->
+            <div class="col-xs-12 col-sm-6 col-md-3 widget--links">
+                <div class="widget--title">
+                    <h5>Company</h5>
+                </div>
+
+                <div class="widget--content">
+                    <ul class="list-unstyled mb-0">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="properties-list.php">Properties</a></li>
+                        <li><a href="sold-properties.php">Sold Property</a></li>
+                        <li><a href="page-contact.php">Contact Us</a></li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <!-- 4️⃣ NEWSLETTER -->
+            <div class="col-xs-12 col-sm-6 col-md-3 widget--newsletter">
+                <div class="widget--title">
+                    <h5>Newsletter</h5>
+                </div>
+
+                <div class="widget--content">
+                    <form class="newsletter--form mb-40" method="post">
+                        <input type="email" name="newsletter_email" class="form-control" 
+                               placeholder="Enter your email" required>
+                        <button type="submit" name="submitn">
+                            <i class="fa fa-paper-plane"></i>
+                        </button>
+                    </form>
+
+                    <small>Subscribe to get latest property updates.</small>
+                </div>
+            </div>
+
+        </div>
     </div>
-    <!-- #wrapper end -->
-
-    <!-- Footer Scripts
+</div>
